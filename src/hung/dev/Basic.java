03adcef4e4533d140bb1433ec494152586f4cfe0
phi hung <njkbin01@gmail.com>
2018-09-18 15:21:42 +0700

    tinh tong 100 so dau tien vong lap for

--- src/hung/dev/Basic.java
+++ src/hung/dev/Basic.java
@@ -4,20 +4,11 @@ public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 4;
-		switch(n)
-		{
-			case 0 : System.out.println("zero!"); break;
-			case 1 : System.out.println("one!"); break;
-			case 2 : System.out.println("two!"); break;
-			case 3 : System.out.println("three!"); break;
-			case 4 : System.out.println("four!"); break;
-			case 5 : System.out.println("five!"); break;
-			case 6 : System.out.println("six!"); break;
-			case 7 : System.out.println("seven!"); break;
-			case 8 : System.out.println("eight!"); break;
-			case 9 : System.out.println("nine!"); break;
-		
+		int tong = 0;
+		for (int value = 1; value <=100; value++) {
+			tong = tong + value;
 		}
+		System.out.println("Tong la:" + tong);
 	}
+
 }
