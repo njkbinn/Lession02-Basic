63683afc7da23834e80dfea8094ab06f0f2844d4
phi hung <njkbin01@gmail.com>
2018-09-18 15:13:34 +0700

    giai phuong trinh bac 2

--- src/hung/dev/Basic.java
+++ src/hung/dev/Basic.java
@@ -4,12 +4,24 @@ public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 5;
-		int b = 8;
-			System.out.println("Tong cua 2 so a va b la: "+(a+b));
-			System.out.println("Hieu cua 2 so a va b la:" +(a-b));
-			System.out.println("Tich cua 2 so a va b la:"+(a*b));
-			System.out.println("Thuong cua 2 so a va b la:"+(a/b));
-	}
-
+		double a=6;
+		double b=10;
+		double c=4;
+		double x,x1,x2;
+		double delta;
+		delta = b*b-4*a*c;
+			if(delta>0) {
+		    x1=(b-Math.sqrt(delta)/2*a);
+		    System.out.println("nghiem x1:"+x1);
+		    x2=(b+Math.sqrt(delta)/2*a);
+		    System.out.println("nghiem x2:"+x2);
+		     		}
+			 else if(delta <0) {
+				 System.out.println("phuong trinh co nghiem");
+			 }
+			 else if(delta ==0) {
+				 x=-b/2*a;
+				 System.out.println("nghiem kep x:"+x);
+			 }
+		}
 }
