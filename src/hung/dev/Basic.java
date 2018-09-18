29d21d6c760c9461ef7e74a5e89dacba4cc4b781
phi hung <njkbin01@gmail.com>
2018-09-18 15:10:23 +0700

    tong hieu tich thuong

---
+++ src/hung/dev/Basic.java
@@ -0,0 +1,15 @@
+package hung.dev;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 8;
+			System.out.println("Tong cua 2 so a va b la: "+(a+b));
+			System.out.println("Hieu cua 2 so a va b la:" +(a-b));
+			System.out.println("Tich cua 2 so a va b la:"+(a*b));
+			System.out.println("Thuong cua 2 so a va b la:"+(a/b));
+	}
+
+}
