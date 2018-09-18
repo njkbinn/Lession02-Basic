08ce48904f242659bf31136b343688e13c4989d9
phi hung <njkbin01@gmail.com>
2018-09-18 15:25:26 +0700

    tinh tong 100 so dau tien vong lap while

--- src/hung/dev/Basic.java
+++ src/hung/dev/Basic.java
@@ -4,11 +4,13 @@ public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int tong = 0;
-		for (int value = 1; value <=100; value++) {
-			tong = tong + value;
+		int i = 0;
+		int sum = 0;
+		while (i <= 100) {
+			sum = sum +i;
+			i++;
 		}
-		System.out.println("Tong la:" + tong);
-	}
+		System.out.println("Tong 100 so la: " +sum);
 
+	}
 }
